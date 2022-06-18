0e0fe03591a02d6ad08606e4d2c851e8eed27715
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-18 14:43:06 +0000

    part1: complete exercise 1.11

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,10 +6,10 @@ const Button = ({ handleClick, text }) => (
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <>
-      {text} {value}
-      <br />
-    </>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
@@ -22,14 +22,16 @@ const Statistics = ({ good, neutral, bad }) => {
     return <div>No feedback given</div>;
   } else {
     return (
-      <div>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={total} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} />
-      </div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + " %"} />
+        </tbody>
+      </table>
     );
   }
 };
