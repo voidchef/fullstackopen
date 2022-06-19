311baff3aebe5199850556779cee8c9af28e1d1b
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-19 13:37:27 +0000

    part2: complete exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (e) => {
+    e.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
+  const handleNameChange = (e) => setNewName(e.target.value);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
