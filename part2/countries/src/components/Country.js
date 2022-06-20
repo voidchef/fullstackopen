34b4a43afe086cf3ef1797235e8eb3fe6917c550
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-20 08:05:44 +0000

    part2: complete exercise 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,16 +1,23 @@
+import Weather from "./Weather";
+
 const Country = ({ countryData }) => {
   return (
     <div>
       <h1>{countryData.name["common"]}</h1>
       <p>capital {countryData.capital}</p>
       <p>area {countryData.area}</p>
-      <h2>languages:</h2>
+      <h3>languages:</h3>
       <ul>
         {Object.values(countryData.languages).map((language) => (
           <li key={language}>{language}</li>
         ))}
       </ul>
       <img src={countryData.flags["png"]} alt="flag" />
+      <h2>Weather in {countryData.name["common"]}</h2>
+      <Weather
+        lat={countryData.capitalInfo["latlng"][0]}
+        lon={countryData.capitalInfo["latlng"][1]}
+      />
     </div>
   );
 };
---
+++ part2/countries/src/components/Weather.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Weather = ({ lat, lon }) => {
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      )
+      .then((response) => {
+        setWeather(response.data);
+        console.log(response.data.main.temp)
+      });
+  }, []);
+
+  return (
+    <div>
+      <p>temperature {weather.main["temp"]} Celsius</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0]["icon"]}@4x.png`}
+        alt={weather.weather[0]["description"]}
+      ></img>
+      <p>wind {weather.wind["speed"]} m/s</p>
+    </div>
+  );
+};
+
+export default Weather;
