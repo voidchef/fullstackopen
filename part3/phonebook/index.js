d5877dffb5be801c93d059d9e23c338e603f9934
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-21 12:17:16 +0000

    part3: complete exercise 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -60,6 +60,18 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons/", (request, response) => {
   const body = request.body;
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
   const person = {
     id: Math.floor(Math.random() * 500),
     name: body.name,
