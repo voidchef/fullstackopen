916ea5fb8465b911e2c9e812080c15ffeda17b16
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-21 12:45:32 +0000

    part3: complete exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,17 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("person", (req) => {
+  if (req.method === "POST") return JSON.stringify(req.body);
+  return null;
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
 
 let persons = [
   {
