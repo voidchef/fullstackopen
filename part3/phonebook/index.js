f503c0fa08167378c9adb00171b1eee09bb316a3
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-21 08:21:14 +0000

    part3: complete exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -29,6 +29,12 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  response.json(person);
+});
+
 app.get("/info", (request, response) => {
   response.send(
     `
